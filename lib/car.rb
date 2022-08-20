dd0fe5f9bd56bfe3a21daddbf300270f2586abe8
Abraham Anyang <anyangmaker90@gmail.com>
2022-08-21 01:12:55 +0300

    commit tree fca386cf649367378d717dd7ebe3cf4facbc053c

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,16 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+class Car < Vehicle
+    def go
+      "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+  end
+
+# Car.superclass
+# Car.superclass.superclass
+# # => Object
+# Car.superclass.superclass.superclass
+# # => BasicObject
+# Car.superclass.superclass.superclass.superclass
+# # => nil
+# puts Integer.superclass
